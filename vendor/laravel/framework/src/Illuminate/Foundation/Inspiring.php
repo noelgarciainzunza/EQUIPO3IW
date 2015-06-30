bebbc77696645b70b024a5b26b417573b3ddb5c3
jlzor <jlz_or@hotmail.com>
2015-06-29 20:55:43 -0600

    Edite Mensaje de Inicio

--- vendor/laravel/framework/src/Illuminate/Foundation/Inspiring.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Inspiring.php
@@ -17,14 +17,10 @@ class Inspiring
     {
         return Collection::make([
 
-            'When there is no desire, all things are at peace. - Laozi',
-            'Simplicity is the ultimate sophistication. - Leonardo da Vinci',
-            'Simplicity is the essence of happiness. - Cedric Bledsoe',
-            'Smile, breathe, and go slowly. - Thich Nhat Hanh',
-            'Simplicity is an acquired taste. - Katharine Gerould',
-            'Well begun is half done. - Aristotle',
-            'He who is contented is rich. - Laozi',
-            'Very little is needed to make a happy life. - Marcus Antoninus',
+            'Jose Luis Ortega Arana - "Soy el Rey"',
+            'Joel - ITC',
+            'Noel. - Ing. Web',
+            'Josue. - Laravel',
 
 
         ])->random();
